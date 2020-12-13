91da56001d8a7a224e2b6f38f0c3705d4479f09f
amit <amith.shambuni@gmail.com>
2020-12-13 11:50:12 +0530

    fst commit with n changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
+const autoprefixer = require('autoprefixer');
 var gulp = require('gulp'),
 watch = require('gulp-watch');
 
+
 gulp.task('styles',function(){
     return gulp.src('./app/assets/style/style.css')
-    .pipe()
+    .pipe([autoprefixer])
     .pipe(gulp.dest('./app/temp/styles'));
 });
 gulp.task('watch',function(){
