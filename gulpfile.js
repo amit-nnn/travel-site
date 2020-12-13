260675d62dfabb91430d688f7c9bed025daf68be
amit <amith.shambuni@gmail.com>
2020-12-13 13:16:14 +0530

    css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-const autoprefixer = require('autoprefixer');
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+postcss = require('gulp-postcss'),
+watch = require('gulp-watch'),
+cssImport = require('postcss-import'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+autoprefixer = require('autoprefixer');
 
 
 gulp.task('styles',function(){
     return gulp.src('./app/assets/style/style.css')
-    .pipe([autoprefixer])
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 gulp.task('watch',function(){
